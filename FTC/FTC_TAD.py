fixed data leakage, added cross-attention to FTC_TAD

FTC/FTC_TAD.py
@@ -8,7 +8,66 @@ import torchvision
 from FTC.posembedding import build_position_encoding
 from FTC.deformable_transformer import DeformableTransformer
 from FTC.util.misc import NestedTensor
+from einops import rearrange
 
+class CrossAttention(nn.Module):
+    """
+    Apply cross attention between x tensor and tab_x tensor.
+
+    Arguments:
+        dim: 
+        context_dim:
+        heads: number of attention heads.
+        dim_head: dimension of q, k, v 
+    
+
+
+    """
+    def __init__(
+        self,
+        dim=1024, #dimension of img input
+        context_dim=1024, #dimension of tab input
+        heads = 12,
+        dim_head = 64,
+        dropout = 0.
+    ):
+        super().__init__()
+        inner_dim = dim_head * heads
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.to_q = nn.Linear(dim, inner_dim, bias = False)
+        self.to_k = nn.Linear(context_dim, inner_dim, bias = False)
+        self.to_v = nn.Linear(context_dim, inner_dim, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, tab_x):
+        
+        #get shape of video input
+        b, f, _ = x.shape
+        h = self.heads
+        kv_input = tab_x
+        # q, k, v = self.to_q(x), self.to_k(kv_input), self.to_v(kv_input)
+        q = self.to_q(x)
+        k = self.to_k(kv_input)
+        v = self.to_v(kv_input)
+        q, k, v = map(lambda t: rearrange(tensor=t, pattern='b f (h d) -> b h f d', h=h), (q, k, v))
+        q = q * self.scale
+        sim = torch.einsum('b h i d, b h j d -> b h i j', q, k)
+
+        # attention
+        attn = sim.softmax(dim = -1) 
+        attn = self.dropout(attn)
+
+        # aggregate
+        out = torch.einsum('b h i j, b h j d -> b h i d', attn, v)
+        
+        # merge heads
+        out = rearrange(out, 'b h f d -> b f (h d)')
+        
+        return self.to_out(out)
 
 class Reduce(nn.Module):
     def __init__(self):
@@ -42,13 +101,25 @@ class Inception(nn.Module):
         
 # New Multi Branch Auto Encoding Transformer
 class FTC(nn.Module):
-    def __init__(self, AE, pos_embed , transformer, embedding_dim, pretrained = False):
+    def __init__(self, 
+                AE, 
+                pos_embed, 
+                transformer, 
+                embedding_dim, 
+                use_tab, 
+                cross_attention, 
+                tab_input_dim, 
+                tab_emb_dims, 
+                pretrained = False):
         super(FTC, self).__init__()
         
         last_features = 4
         self.pretrained = pretrained
+        self.use_tab = use_tab
+        self.tab_input_dim = tab_input_dim
+        self.tab_emb_dims = tab_emb_dims
         
-        
+        self.cross_attention = cross_attention
         self.AE = AE
         if self.pretrained == True:
             from pathlib import Path
@@ -62,6 +133,14 @@ class FTC(nn.Module):
 
         self.pos_embed = pos_embed
         self.transformer = transformer
+        self.tab_embed = nn.Sequential(
+            nn.Linear(in_features=self.tab_input_dim, out_features=self.tab_emb_dims[0]),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=self.tab_emb_dims[0], out_features=tab_emb_dims[1]),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=tab_emb_dims[1], out_features=tab_emb_dims[2]),
+            nn.ReLU(inplace=True)
+        )
         
         self.aorticstenosispred = nn.Sequential(
             nn.Linear(in_features=embedding_dim, out_features=embedding_dim//2, bias=True),
@@ -84,7 +163,7 @@ class FTC(nn.Module):
         #self.vf = frames_per_video
         self.em = embedding_dim
         
-    def forward(self, x):   
+    def forward(self, x, tab_x):   
         # Video dimension (B x F x C x H x W)
         x = x.permute(0,2,1,3,4)
         
@@ -112,9 +191,19 @@ class FTC(nn.Module):
         pos = self.pos_embed(samples).cuda()
         
         #  B x F x Emb
-
         outputs = self.transformer([embeddings_reshaped],[pos],[mask])
-        
+
+        #integrate tab data
+        if self.use_tab:
+            tab_x = torch.unsqueeze(tab_x, dim=-1) 
+            
+            # B x F x 1
+            #Tranform x feature values to higher dim using a shared mlp layer
+            tab_x = self.tab_embed(tab_x)
+
+            #cross attention between video and tabular embeddings
+            outputs = self.cross_attention(outputs, tab_x)
+
         method = "attention"   # "average","emb_mag","attention",
         if method == "average":
             # B x F x Emb => B x T x 4
@@ -159,12 +248,16 @@ class FTC(nn.Module):
 
         return as_prediction,entropy_attention,outputs,att_weight
 
-def get_model_tad(emb_dim, img_per_video, 
+def get_model_tad(emb_dim, 
+              tab_input_dim, 
+              tab_emb_dims, 
+              img_per_video, 
               num_hidden_layers = 16,
               intermediate_size = 8192,
               rm_branch = None,
               use_conv = False,
-              attention_heads=16
+              attention_heads=16,
+              use_tab=True
               ):
     
     model_res = torchvision.models.resnet18()
@@ -184,7 +277,21 @@ def get_model_tad(emb_dim, img_per_video,
             num_feature_levels=1,
             dec_n_points=4,
             enc_n_points=4)
+    
+    cross_attention = CrossAttention(dim=1024, 
+        context_dim=tab_emb_dims[2], 
+        heads = 12,
+        dim_head = 64,
+        dropout = 0.)
         
-    model = FTC(model_res,pos_embed,transformer, emb_dim,pretrained = False) 
+    model = FTC(AE=model_res,
+                pos_embed=pos_embed,
+                transformer=transformer, 
+                embedding_dim=emb_dim, 
+                use_tab=use_tab, 
+                cross_attention=cross_attention, 
+                tab_input_dim=tab_input_dim, 
+                tab_emb_dims=tab_emb_dims, 
+                pretrained=False) 
     
     return model

dataloader/as_dataloader.py
@@ -16,7 +16,7 @@ from random import lognormvariate
 from random import seed
 import torch.nn as nn
 import random
-from dataloader.utils import load_as_data, preprocess_as_data
+from dataloader.utils import load_as_data, preprocess_as_data, fix_leakage
 
 seed(42)
 torch.random.manual_seed(42)
@@ -110,6 +110,7 @@ def get_as_dataloader(config, split, mode):
         fr = 16
     
     # read in the data directory CSV as a pandas dataframe
+    raw_dataset = pd.read_csv(img_path_dataset)
     dataset = pd.read_csv(img_path_dataset)
         
     # append dataset root to each path in the dataframe
@@ -150,6 +151,14 @@ def get_as_dataloader(config, split, mode):
         tab_dataset = train_set
     elif split != 'all':
         raise ValueError(f'View should be train/val/test/all, got {split}')
+    
+    #Fix data leakage 
+    if split == 'train':
+        dataset = fix_leakage(df=raw_dataset, df_subset=dataset, split=split)
+    elif split == 'val':
+        dataset = fix_leakage(df=raw_dataset, df_subset=dataset, split=split)
+    elif split == 'test':
+        dataset = fix_leakage(df=raw_dataset, df_subset=dataset, split=split)
         
     dset = AorticStenosisDataset(img_path_dataset=dataset, 
                                 tab_dataset=tab_dataset,
@@ -284,11 +293,11 @@ class AorticStenosisDataset(Dataset):
         #get associated tabular data based on echo ID
         study_num = data_info['Echo ID#']
         tab_info = self.tab_dataset.loc[int(study_num)]
-        tab_info = torch.tensor(tab_info.values)
-        #If study num for tabular dataset does not exist, print out exception...
-            #TODO
+        tab_info = torch.tensor(tab_info.values, dtype=torch.float32)
 
         cine_original = self.cine_loader(data_info['path'])
+
+        #if raw_dataset comes from all_cines in nas drive...
         folder = 'round2'
         if folder == 'all_cines':
             cine_original = cine_original.transpose((2,0,1))
@@ -298,7 +307,6 @@ class AorticStenosisDataset(Dataset):
         window_length = 60000 / (lognormvariate(self.hr_mean, self.hr_srd) * data_info['frame_time'])
         cine = self.get_random_interval(cine_original, window_length)
         #print(cine.shape)
-        #cine = resize(cine, (self.frames, *self.resolution))
         cine = resize(cine, (32, *self.resolution))
         cine = torch.tensor(cine).unsqueeze(0)
         

network.py
@@ -17,9 +17,6 @@ from utils import validation_constructive
 from pathlib import Path
 from sklearn.metrics import confusion_matrix
 
-
-
-
 class TransformerFeatureMap:
     def __init__(self, model, layer_name='avgpool'):
         self.model = model
@@ -82,8 +79,8 @@ class Network(object):
         else:
             self.optimizer = torch.optim.Adam(self.model.parameters(), lr=config['lr'])
 
-        self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-            self.optimizer,T_max=config['num_epochs'])
+        self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer,
+                                                                    T_max=config['num_epochs'])
         self.loss_type = config['loss_type']
         self.contrastive_method = config['cotrastive_method']
         self.temperature = config['temp']
@@ -118,15 +115,34 @@ class Network(object):
 
         # File that we save trained model into.
         self.checkpts_file = os.path.join(self.log_dir, "checkpoint.pth")
-
         # We save model that achieves the best performance: early stopping strategy.
-        self.bestmodel_file = Path(self.config['best_model_path'])
+        self.best_model_dir = self.config['best_model_dir']
+        if not os.path.exists(self.best_model_dir):
+            os.makedirs(self.best_model_dir)      
+        self.bestmodel_file = os.path.join(self.best_model_dir, 'best_model.pth')
         self.bestmodel_file_contrastive = os.path.join(self.log_dir, "best_model_cont.pth")
-        #TODO - change paths
-        
         # For test, we also save the results dataframe
         self.test_results_file = os.path.join(self.log_dir, "best_model.pth")
- 
+
+        #TODO
+        # if self.config['use_tab']:
+        #     self.checkpts_file = os.path.join(self.log_dir, "multimodal_checkpoint.pth")
+
+        # if self.config['use_tab']:
+        #     self.bestmodel_file = os.path.join(self.best_model_dir, 'best_multimodal_model.pth')
+        # else:
+        #     self.bestmodel_file = os.path.join(self.best_model_dir, 'best_model.pth')
+
+        # if self.config['use_tab']:
+        #     self.bestmodel_file_contrastive = os.path.join(self.log_dir, "best_multimodalmodel_cont.pth")
+        # else:
+        #     self.bestmodel_file_contrastive = os.path.join(self.log_dir, "best_model_cont.pth")
+
+        # if self.config['use_tab']:
+        #     self.test_results_file = os.path.join(self.log_dir, "best_multimodal_model.pth")
+        # else:
+        #     self.test_results_file = os.path.join(self.log_dir, "best_model.pth")
+
     
     def _save(self, pt_file):
         """Saving trained model."""
@@ -134,7 +150,6 @@ class Network(object):
         # Save the trained model.
         torch.save(
             {
-                #"model": self.model.module.state_dict(),
                 "model": self.model.state_dict(),
                 "optimizer": self.optimizer.state_dict(),
             },
@@ -203,9 +218,6 @@ class Network(object):
         uni = utils.test_unimodality(prob.cpu().numpy())
         return argm, max_percentage, entropy, vacuity, uni
         
-    # def get_lr(self):
-    #     return self.scheduler.get_lr()
-        
     def train(self, loader_tr, loader_va,loader_te):
         """Training pipeline."""
         # Switch model into train mode.
@@ -213,7 +225,6 @@ class Network(object):
         best_va_acc = 0.0 # Record the best validation metrics.
         best_va_acc_supcon = 0.0
         best_cont_loss = 1000
-        #best_B_f1 = 0.0
             
         for epoch in range(self.config['num_epochs']):
             #losses_AS = []
@@ -240,8 +251,7 @@ class Network(object):
                             target_AS = target_AS.cuda()
                             target_B = target_B.cuda()
                         if self.config['model'] == "FTC_TAD":
-                            pred_AS,entropy_attention,outputs, _ = self.model(cine) # Bx3xTxHxW
-                            #the output for FTC-TAD is this: as_prediction,entropy_attention,outputs,att_weight
+                            pred_AS,entropy_attention,outputs, _ = self.model(cine, tab_data) # Bx3xTxHxW
                             
                             # Calculating temporal coherent npair loss
                             similarity_matrix = (torch.bmm(outputs,outputs.permute((0,2,1)))/1024)
@@ -264,6 +274,7 @@ class Network(object):
                         cines = torch.cat([cine[0], cine[1]], dim=0)
                         if self.config['use_cuda']:
                             cines = cines.cuda()
+                            tab_data = tab_data.cuda()
                             target_AS = target_AS.cuda()
                             target_B = target_B.cuda()
                         bsz = target_AS.shape[0]
@@ -294,13 +305,13 @@ class Network(object):
             loss_avg = torch.mean(torch.stack(losses)).item()
             #acc_AS, f1_B, val_loss = self.test(loader_va, mode="val")
             if self.config['cotrastive_method'] == 'CE' or self.config['cotrastive_method'] == 'Linear':
-                ## Validation Data Evaluration 
+                ## Validation Data Evaluation 
                 acc_AS, val_loss = self.test(loader_va, mode="val")
                 if self.config['use_wandb']:
                     wandb.log({"tr_loss":loss_avg, "val_loss":val_loss, "val_AS_acc":acc_AS})
                     # wandb.log({"tr_loss_AS":loss_avg_AS, "tr_loss_B":loss_avg_B, "tr_loss":loss_avg,
                     #            "val_loss":val_loss, "val_B_f1":f1_B, "val_AS_acc":acc_AS})
-                ## Test Data Evaluration    
+                ## Test Data Evaluation    
                 acc_AS_te, te_loss = self.test(loader_te, mode="val")
                 if self.config['use_wandb']:
                     wandb.log({ "te_loss":te_loss, "test_AS_acc":acc_AS_te})
@@ -370,6 +381,7 @@ class Network(object):
         losses = []
         for data in tqdm(loader_te):
             cine = data[0]
+            tab_data = data[1]
             target_AS = data[2]
             target_B = data[3]
             # Transfer data from CPU to GPU.
@@ -381,13 +393,14 @@ class Network(object):
                         cine = [c.cuda() for c in cine]
                     else:
                         cine = cine.cuda()
+                    tab_data = tab_data.cuda()
                     target_AS = target_AS.cuda()
                     target_B = target_B.cuda()
                     
                 if self.config['model'] == "FTC_TAD":
-                    pred_AS,entropy_attention,outputs = self.model(cine) # Bx3xTxHxW
+                    pred_AS, _, _, _ = self.model(cine, tab_data) # Bx3xTxHxW
                 else:
-                    pred_AS = self.model(cine) # Bx3xTxHxW
+                    pred_AS = self.model(cine, tab_data) # Bx3xTxHxW
                 loss = self._get_loss(pred_AS, target_AS, self.num_classes_AS)
                 losses += [loss]
 
@@ -396,6 +409,7 @@ class Network(object):
                 cines = torch.cat([cine[0], cine[1]], dim=0)
                 if self.config['use_cuda']:
                     cines = cines.cuda()
+                    tab_data = tab_data.cuda()
                     target_AS = target_AS.cuda()
                     target_B = target_B.cuda()
                 bsz = target_AS.shape[0]
@@ -449,13 +463,14 @@ class Network(object):
         predicted_qual = []
         embeddings = []
         
-        for cine, target_AS, target_B, data_info, cine_orig in tqdm(loader):
+        for cine, tab_info, target_AS, target_B, data_info, cine_orig in tqdm(loader):
             # collect the label info
             target_AS_arr.append(int(target_AS[0]))
             target_B_arr.append(int(target_B[0]))
             # Transfer data from CPU to GPU.
             if self.config['use_cuda']:
                 cine = cine.cuda()
+                tab_info = tab_info.cuda()
                 target_AS = target_AS.cuda()
                 target_B = target_B.cuda()
                 
@@ -474,7 +489,7 @@ class Network(object):
             # get the model prediction
             # pred_AS, pred_B = self.model(cine) #1x3xTxHxW
             if self.config['model'] == "FTC_TAD":
-                pred_AS,entropy_attention,outputs = self.model(cine) # Bx3xTxHxW
+                pred_AS,entropy_attention,outputs, att_weight = self.model(cine, tab_info) # Bx3xTxHxW
             else:
                 pred_AS = self.model(cine) # Bx3xTxHxW
             # collect the model prediction info