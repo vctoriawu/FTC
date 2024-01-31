fixed data leakage, added cross-attention to FTC_TAD

FTC/FTC_TAD.py
@@ -163,7 +163,7 @@ class FTC(nn.Module):
         #self.vf = frames_per_video
         self.em = embedding_dim
         
-    def forward(self, x, tab_x):   
+    def forward(self, x, tab_x, split):   
         # Video dimension (B x F x C x H x W)
         x = x.permute(0,2,1,3,4)
         
@@ -194,15 +194,18 @@ class FTC(nn.Module):
         outputs = self.transformer([embeddings_reshaped],[pos],[mask])
 
         #integrate tab data
-        if self.use_tab:
-            tab_x = torch.unsqueeze(tab_x, dim=-1) 
-            
-            # B x F x 1
-            #Tranform x feature values to higher dim using a shared mlp layer
-            tab_x = self.tab_embed(tab_x)
+        if split=='Train':
+            if self.use_tab:
+                tab_x = torch.unsqueeze(tab_x, dim=-1) 
+                
+                # B x F x 1
+                #Tranform x feature values to higher dim using a shared mlp layer
+                tab_x = self.tab_embed(tab_x)
 
-            #cross attention between video and tabular embeddings
-            outputs = self.cross_attention(outputs, tab_x)
+                #cross attention between video and tabular embeddings
+                outputs = self.cross_attention(outputs, tab_x)
+        else:
+            print("Cross-attention module has been skipped.")
 
         method = "attention"   # "average","emb_mag","attention",
         if method == "average":

network.py
@@ -251,7 +251,7 @@ class Network(object):
                             target_AS = target_AS.cuda()
                             target_B = target_B.cuda()
                         if self.config['model'] == "FTC_TAD":
-                            pred_AS,entropy_attention,outputs, _ = self.model(cine, tab_data) # Bx3xTxHxW
+                            pred_AS,entropy_attention,outputs, _ = self.model(cine, tab_data, split='Train') # Bx3xTxHxW
                             
                             # Calculating temporal coherent npair loss
                             similarity_matrix = (torch.bmm(outputs,outputs.permute((0,2,1)))/1024)
@@ -266,7 +266,7 @@ class Network(object):
                             losses += [loss] 
                         
                         else:
-                            pred_AS = self.model(cine) # Bx3xTxHxW
+                            pred_AS = self.model(cine, tab_data, split='Train') # Bx3xTxHxW
                             loss = self._get_loss(pred_AS, target_AS, self.num_classes_AS)
                             losses += [loss]
                     # Contrastive Learning
@@ -278,7 +278,7 @@ class Network(object):
                             target_AS = target_AS.cuda()
                             target_B = target_B.cuda()
                         bsz = target_AS.shape[0]
-                        features = self.model(cines) # Bx3xTxHxW
+                        features = self.model(cines, tab_data, split='Train') # Bx3xTxHxW
                         f1, f2 = torch.split(features, [bsz, bsz], dim=0)
                         features = torch.cat([f1.unsqueeze(1), f2.unsqueeze(1)], dim=1)
                         if self.config['cotrastive_method'] == 'SupCon':
@@ -398,9 +398,9 @@ class Network(object):
                     target_B = target_B.cuda()
                     
                 if self.config['model'] == "FTC_TAD":
-                    pred_AS, _, _, _ = self.model(cine, tab_data) # Bx3xTxHxW
+                    pred_AS, _, _, _ = self.model(cine, tab_data, split='Test') # Bx3xTxHxW
                 else:
-                    pred_AS = self.model(cine, tab_data) # Bx3xTxHxW
+                    pred_AS = self.model(cine, tab_data, split='Test') # Bx3xTxHxW
                 loss = self._get_loss(pred_AS, target_AS, self.num_classes_AS)
                 losses += [loss]
 
@@ -489,9 +489,9 @@ class Network(object):
             # get the model prediction
             # pred_AS, pred_B = self.model(cine) #1x3xTxHxW
             if self.config['model'] == "FTC_TAD":
-                pred_AS,entropy_attention,outputs, att_weight = self.model(cine, tab_info) # Bx3xTxHxW
+                pred_AS,entropy_attention,outputs, att_weight = self.model(cine, tab_info, split='Test') # Bx3xTxHxW
             else:
-                pred_AS = self.model(cine) # Bx3xTxHxW
+                pred_AS = self.model(cine, tab_info, split='Test') # Bx3xTxHxW
             # collect the model prediction info
             argm, max_p, ent, vac, uni = self._get_prediction_stats(pred_AS, self.num_classes_AS)
             pred_AS_arr.append(argm.cpu().numpy()[0])